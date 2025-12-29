7724c8f9bb549a837e9969aedbe5a629f47259de
EnricoGollner <enricogollner.tech@gmail.com>
2025-12-29 20:08:45 -0300

    Backend created

---
+++ backend/src/test/java/dev/enricogollner/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.enricogollner.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
